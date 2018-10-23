b79e3dee65e440257e26153a7cfd5650094f215b
Raymond Chao <rchao4@illinois.edu>
2018-10-23 11:45:59 -0500

    rendered drilldown

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import SampleComponent from './components/SampleComponent'
 import NestedDropdown from './components/NestedDropdown'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <SampleComponent  /> */}
         <NestedDropdown />
       </div>
     );
--- src/components/NestedDropdown.js
+++ src/components/NestedDropdown.js
@@ -1,47 +1,106 @@
-import React, {Component} from 'react';
-import DropdownMenu, { NestedDropdownMenu } from 'react-dd-menu';
+import React, { Component } from 'react'
+import { Dropdown, Icon, Menu } from 'semantic-ui-react'
 
-
-
-export default class NestedDropdown extends Component {
-
-  state = { isMenuOpen: false };
-
-  toggle = () => {
-    this.setState({ isMenuOpen: !this.state.isMenuOpen });
+const dummyData = {
+  "forms": {
+    "all": {
+      "status": {
+        "Triag": "Triag",
+        "Pending More Information": "Pending More Information",
+        "Jira|": "Jira",
+        "Rejected": "Rejected",
+        "Complete": "Complete"
+      },
+      "program_area": {
+        "4-H": "4-H",
+        "Community and Economic Development": "Community and Economic Development",
+        "Illinois Nutritional Education Program": "Illinois Nutritional Education Program",
+        "Family and Consumer Sciences": "Family and Consumer Sciences",
+        "Agrictulture and Natural Resources": "Agrictulture and Natural Resources",
+        "County Administration": "County Administration",
+        "State Extension": "State Extension"
+      },
+      "offices": {
+        "494": "Alexander/Johnson/Massac/Pulaski/Union Unit {494 - UM}",
+        "481": "Adams/Brown/Hancock/Pike/Schuyler Unit {481 - UM}",
+        "488": "Bond/Clinton/Jefferson/Marion/Washington Unit {488 - UM}"
+      }
+    }  
   }
+}
 
-  close = () => {
-    this.setState({ isMenuOpen: false });
-  };
+export default class MenuExampleSubMenu extends Component {
+  //defines initial empty state
+  state = {}
 
-  click = () => {
-    console.log('You clicked an item');
-  };
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
-    const menuOptions = {
-      isOpen: this.state.isMenuOpen,
-      close: this.close,
-      toggle: <button type="button" onClick={this.toggle}>Click me!</button>,
-      align: 'right',
-    };
+    console.log("state", this.state)
+    console.log("dummy", dummyData);
 
-    const nestedProps = {
-      toggle: <a href="woopee.com">Hover me for Nested Menu!</a>,
-      animate: true,
-    };
+    //creates new state called 'activeItem'
+    const { activeItem } = this.state
 
     return (
-      <DropdownMenu {...menuOptions}>
-        <li><a href="facebook.com">Example 1</a></li>
-        <li><button type="button" onClick={this.click}>Example 2</button></li>
-        <li role="separator" className="separator" />
-        <NestedDropdownMenu {...nestedProps}>
-          <li><a href="google.com">I am in a Nested Menu!</a></li>
-        </NestedDropdownMenu>
-      </DropdownMenu>
-    );
-  }
+      <Menu vertical>
 
+        <Menu.Item>
+          Home
+          <Menu.Menu>
+            <Menu.Item
+              name='search'
+              active={activeItem === 'search'}
+              onClick={this.handleItemClick}
+            >
+              Search
+            </Menu.Item>
+            <Menu.Item name='add' active={activeItem === 'add'} onClick={this.handleItemClick}>
+              Add
+            </Menu.Item>
+            <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick}>
+              Remove
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu.Item>
+
+        <Menu.Item name='browse' active={activeItem === 'browse'} onClick={this.handleItemClick}>
+          <Icon name='grid layout' />
+          Browse
+        </Menu.Item>
+        <Menu.Item
+          name='messages'
+          active={activeItem === 'messages'}
+          onClick={this.handleItemClick}
+        >
+          Messages
+        </Menu.Item>
+
+        <Dropdown item text='More'>
+          {/* <Dropdown.Menu>
+            <Dropdown.Item icon='edit' text='Edit Profile' />
+            <Dropdown.Item icon='globe' text='Choose Language' />
+            <Dropdown.Item icon='settings' text='Account Settings' />
+              <Dropdown item text='Even More!'>
+                <Dropdown.Menu>
+                  <Dropdown.Item icon='globe' text='World Dom' />
+                </Dropdown.Menu>
+              </Dropdown>
+          </Dropdown.Menu> */}
+
+          {/*
+          Recursively iterate over the drilldown? Or let state handle it?
+          */}
+          
+          <Dropdown.Menu>
+            {Object.keys(dummyData).map(e => {
+              return (
+                <Dropdown.Item text={e} />
+              )
+            })}
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu>
+    )
+  }
 }
