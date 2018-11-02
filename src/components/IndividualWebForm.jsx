f58e63e8b6ea1b221242957d2e3600a2411fbdfa
Raymond Chao <rchao4@illinois.edu>
2018-11-02 13:48:44 -0500

    updated individual webform with bulma

--- src/components/IndividualWebForm.jsx
+++ src/components/IndividualWebForm.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
-
-
+import 'bulma/css/bulma.css'
 
 
 export default class IndividualForm extends Component {
@@ -13,33 +12,47 @@ export default class IndividualForm extends Component {
     render() {
         return(
             <div>
-                <h1> Individual Webform </h1>
-                <form class="ui form">
-                    <div class="field">
-                        <label>First Name</label>
-                        <input type="text" name="first-name" placeholder="First Name" />
+                <h1>Individual WebForm</h1>
+                
+                <div class="field">
+                <label class="label">First Name</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="First Name" />
                     </div>
-                    <div class="field">
-                        <label>Last Name</label>
-                        <input type="text" name="last-name" placeholder="Last Name" />
+                </div>
+
+                <div class="field">
+                <label class="label">Last Name</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Last Name" />
                     </div>
-                    <div class="field">
-                        <label>Email</label>
-                        <input type="text" name="email" placeholder="Email" />
+                </div>
+
+                <div class="field">
+                <label class="label">Email</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Email" />
                     </div>
-                    <div class="field">
-                        <label>Sex</label>
-                        <div class="ui selection dropdown">
-                            <input type="hidden" name="gender" />
-                            <i class="dropdown icon"></i>
-                            <div class="default text">Gender</div>
-                            <div class="menu">
-                                <div class="item" data-value="1">Male</div>
-                                <div class="item" data-value="0">Female</div>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+                </div>
+
+                <label class="label"> Gender </label>
+                <div class="select">
+                    <select>
+                        <option>Male</option>
+                        <option>Female</option>
+                    </select>
+                </div>
+
+                <label class="label"> Age </label>
+                <div class="select is-multiple">
+                    <select multiple size="4">
+                        <option value="Argentina">less than 5 Years</option>
+                        <option value="Bolivia">5-17 years</option>
+                        <option value="Brazil">18-59 years</option>
+                        <option value="Chile">60 years or Older</option>
+                    </select>
+                </div>
+
             </div>
             
         )
--- src/components/NestedDropdown.js
+++ src/components/NestedDropdown.js
@@ -1,134 +1,134 @@
-import React, { Component } from 'react'
-import { Dropdown, Icon, Menu } from 'semantic-ui-react'
+// import React, { Component } from 'react'
+// import { Dropdown, Icon, Menu } from 'semantic-ui-react'
 //need to convert this json object into usable?
-const dummyData = {
-  "forms": {
-    "all": {
-      "status": {
-        "Triag": "Triag",
-        "Pending More Information": "Pending More Information",
-        "Jira|": "Jira",
-        "Rejected": "Rejected",
-        "Complete": "Complete"
-      },
-      "program_area": {
-        "4-H": "4-H",
-        "Community and Economic Development": "Community and Economic Development",
-        "Illinois Nutritional Education Program": "Illinois Nutritional Education Program",
-        "Family and Consumer Sciences": "Family and Consumer Sciences",
-        "Agrictulture and Natural Resources": "Agrictulture and Natural Resources",
-        "County Administration": "County Administration",
-        "State Extension": "State Extension"
-      },
-      "offices": {
-        "494": "Alexander/Johnson/Massac/Pulaski/Union Unit {494 - UM}",
-        "481": "Adams/Brown/Hancock/Pike/Schuyler Unit {481 - UM}",
-        "488": "Bond/Clinton/Jefferson/Marion/Washington Unit {488 - UM}"
-      }
-    }  
-  }
-}
-// formatted JSON object should be in this form.
-//can probably use Object.keys(object) to convert to array
-var item=[
-  {
-      name: '1',
-      child: []
-  },
-  {
-    name: '2',
-    child: [
-      {
-        name: '2.1',
-        child: [
-          {
-            name: '2.1.1',
-            child:[
-              {
-                name: '2.1.1.1',
-                child: [],
-              }
-            ]
-          }
-        ]
-      },
-      {
-        name: '2.2',
-        child: []
-      }
-    ]
-  }
-];
+// const dummyData = {
+//   "forms": {
+//     "all": {
+//       "status": {
+//         "Triag": "Triag",
+//         "Pending More Information": "Pending More Information",
+//         "Jira|": "Jira",
+//         "Rejected": "Rejected",
+//         "Complete": "Complete"
+//       },
+//       "program_area": {
+//         "4-H": "4-H",
+//         "Community and Economic Development": "Community and Economic Development",
+//         "Illinois Nutritional Education Program": "Illinois Nutritional Education Program",
+//         "Family and Consumer Sciences": "Family and Consumer Sciences",
+//         "Agrictulture and Natural Resources": "Agrictulture and Natural Resources",
+//         "County Administration": "County Administration",
+//         "State Extension": "State Extension"
+//       },
+//       "offices": {
+//         "494": "Alexander/Johnson/Massac/Pulaski/Union Unit {494 - UM}",
+//         "481": "Adams/Brown/Hancock/Pike/Schuyler Unit {481 - UM}",
+//         "488": "Bond/Clinton/Jefferson/Marion/Washington Unit {488 - UM}"
+//       }
+//     }  
+//   }
+// }
+// // formatted JSON object should be in this form.
+// //can probably use Object.keys(object) to convert to array
+// var item=[
+//   {
+//       name: '1',
+//       child: []
+//   },
+//   {
+//     name: '2',
+//     child: [
+//       {
+//         name: '2.1',
+//         child: [
+//           {
+//             name: '2.1.1',
+//             child:[
+//               {
+//                 name: '2.1.1.1',
+//                 child: [],
+//               }
+//             ]
+//           }
+//         ]
+//       },
+//       {
+//         name: '2.2',
+//         child: []
+//       }
+//     ]
+//   }
+// ];
 
-export default class MenuExampleSubMenu extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    }
-  }
+// export default class MenuExampleSubMenu extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     }
+//   }
 
-  handleItemClick = (e, { name }) => {
-    this.setState({ activeItem: name })
-  }
+//   handleItemClick = (e, { name }) => {
+//     this.setState({ activeItem: name })
+//   }
 
 
-  //recursive dropdown menu
-  //needs to follow a certain format for response JSON object
-  _createDropdown = (item) => {
-    return item.map((elem, i) => {
-      return (
-        <Dropdown item text={elem.name} key={elem.name}>
-          <Dropdown.Menu key={elem.name}>
-            <Dropdown.Item text={elem.name} />
-            {elem.child.length ? this._createDropdown(elem.child) : null}
-          </Dropdown.Menu>
-        </Dropdown>
-      )
-    })
-  }
+//   //recursive dropdown menu
+//   //needs to follow a certain format for response JSON object
+//   _createDropdown = (item) => {
+//     return item.map((elem, i) => {
+//       return (
+//         <Dropdown item text={elem.name} key={elem.name}>
+//           <Dropdown.Menu key={elem.name}>
+//             <Dropdown.Item text={elem.name} />
+//             {elem.child.length ? this._createDropdown(elem.child) : null}
+//           </Dropdown.Menu>
+//         </Dropdown>
+//       )
+//     })
+//   }
 
-  render() {
+//   render() {
 
-    //creates new piece of state called 'activeItem'
-    const { activeItem } = this.state
+//     //creates new piece of state called 'activeItem'
+//     const { activeItem } = this.state
 
-    return (
-      <Menu vertical>
+//     return (
+//       <Menu vertical>
 
-        <Menu.Item>
-          Home
-          <Menu.Menu>
-            <Menu.Item
-              name='search'
-              active={activeItem === 'search'}
-              onClick={this.handleItemClick}
-            >
-              Search
-            </Menu.Item>
-            <Menu.Item name='add' active={activeItem === 'add'} onClick={this.handleItemClick}>
-              Add
-            </Menu.Item>
-            <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick}>
-              Remove
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu.Item>
+//         <Menu.Item>
+//           Home
+//           <Menu.Menu>
+//             <Menu.Item
+//               name='search'
+//               active={activeItem === 'search'}
+//               onClick={this.handleItemClick}
+//             >
+//               Search
+//             </Menu.Item>
+//             <Menu.Item name='add' active={activeItem === 'add'} onClick={this.handleItemClick}>
+//               Add
+//             </Menu.Item>
+//             <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick}>
+//               Remove
+//             </Menu.Item>
+//           </Menu.Menu>
+//         </Menu.Item>
 
-        <Menu.Item name='browse' active={activeItem === 'browse'} onClick={this.handleItemClick}>
-          <Icon name='grid layout' />
-          Browse
-        </Menu.Item>
-        <Menu.Item
-          name='messages'
-          active={activeItem === 'messages'}
-          onClick={this.handleItemClick}
-        >
-          Messages
-        </Menu.Item>
+//         <Menu.Item name='browse' active={activeItem === 'browse'} onClick={this.handleItemClick}>
+//           <Icon name='grid layout' />
+//           Browse
+//         </Menu.Item>
+//         <Menu.Item
+//           name='messages'
+//           active={activeItem === 'messages'}
+//           onClick={this.handleItemClick}
+//         >
+//           Messages
+//         </Menu.Item>
 
-          {this._createDropdown(item)}
+//           {this._createDropdown(item)}
         
-      </Menu>
-    )
-  }
-}
+//       </Menu>
+//     )
+//   }
+// }
